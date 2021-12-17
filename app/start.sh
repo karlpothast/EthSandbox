420eeb5d62d0fa3d976548ac364b610b9cf2827c
Karl Pothast <karlpothast@gmail.com>
2021-12-16 19:57:59 -0600

    remove old names

--- app/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-screen -S 'x11' -d -m /app/conf.d/x.sh
-screen -S 'novnc' -d -m /app/conf.d/web.sh
--- app/x.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-fuser -k 5900/tcp
-x11vnc -create -env FD_PROG=/usr/bin/fluxbox \
-    -env X11VNC_FINDDISPLAY_ALWAYS_FAILS=1 \
-        -env X11VNC_CREATE_GEOM=${1:-1024x768x16} \
-        -nopw -forever -shared -bg
